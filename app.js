f96f7f472bbf16c1aaa81275a2ce423e414c8f66
camille <camilledds0@gmail.com>
2025-11-08 21:35:18 -0300

    Testando criação de rotas

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.get("/", function(req, res){
+    res.send("Mensagem - teste de rota");
+});
+
+app.get("/rota2", function(req,res){
+    res.send("Mensagem - teste de rota 2");
+});
+
+app.get("/rota3", function(req,res){
+    res.send("Mensagem - teste de rota 3");
+});
+
+app.get("/rota_segura", function(req, res){
+    if(req.params.id == "1"){
+        res.send("Parabens você está agora na rota segura do site!");
+    }
+});
+
 app.listen(3000, function(){
-    console.log("servidor rodando🔥")
+    console.log("Servidor rodando")
 });
