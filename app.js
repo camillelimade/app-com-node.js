5c2e7d374692c9c90f675c1464a9fdd1585d67d6
camille <camilledds0@gmail.com>
2025-11-22 23:24:08 -0300

    Criando estrutura de cadastros dinamicos

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 const express = require('express');
 const app = express();
+const Produtos = require("./models/Produtos");
+
+app.post("/cadastro", function(req,res){
+   Produtos.create({
+        nome: req.body.nome,
+        preco: req.body.preco,
+        descricao: req.body.descricao
+    });then(function(){
+        res.send("Produto cadastrado!");
+    }).catch(function(erro){
+        res.send("Erro ao cadastrar produto " + erro);
+    })
+});
 
 app.get("/", function(req, res){
     res.send("Mensagem - teste de rota");
 });
 
-app.get("/rota2", function(req,res){
-    res.send("Mensagem - teste de rota 2");
-});
+// app.get("/rota2", function(req,res){
+//     res.send("Mensagem - teste de rota 2");
+// });
 
-app.get("/rota3", function(req,res){
-    res.send("Mensagem - teste de rota 3");
-});
+// app.get("/rota3", function(req,res){
+//     res.send("Mensagem - teste de rota 3");
+// });
 
-app.get("/rota55/:id", function(req, res){
-    if(req.params.id == 1){
-        res.send("Parabens você está agora na primeira rota segura do site!");
-    }else if(req.params.id == 2){
-        res.send("Parabens você está agora na segunda rota segura do site!");
-    }else{
-        res.send("Nenhuma rota encontrada")
-    }
-});
+// app.get("/rota55/:id", function(req, res){
+//     if(req.params.id == 1){
+//         res.send("Parabens você está agora na primeira rota segura do site!");
+//     }else if(req.params.id == 2){
+//         res.send("Parabens você está agora na segunda rota segura do site!");
+//     }else{
+//         res.send("Nenhuma rota encontrada")
+//     }
+// });
 
 app.listen(3000, function(){
     console.log("Servidor rodando")
